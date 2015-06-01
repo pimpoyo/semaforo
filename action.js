97b05e943034ff631aafea53ce5284f6e6be58a9
Alumnos <alumnos@iMac-lab07.local>
2015-06-01 12:03:16 +0200

    Fix js with classes into style.backgroundColor

--- action.js
+++ action.js
@@ -1,19 +1,23 @@
 count = 0;
 document.getElementById("changeButton").addEventListener("click", changeColor );
 function changeColor() {
-	colorValue = ++count%3;
+	colorValue = ++count%3; //if trafficLight has 3 light
+	off = "circle off";
+	green = "circle green";
+	red = "circle red";
+	orange = "circle orange";
 	
 	if (colorValue == 0) {
-		document.getElementById("top").style.backgroundColor = "#555555";
-		document.getElementById("center").style.backgroundColor = "#555555";
-		document.getElementById("bottom").style.backgroundColor = "#FF0000";
+		document.getElementById("top").className = off;
+		document.getElementById("center").className = off;
+		document.getElementById("bottom").className = red;
 	} else if (colorValue == 1) {
-		document.getElementById("top").style.backgroundColor = "#00FF00";
-		document.getElementById("center").style.backgroundColor = "#555555";
-		document.getElementById("bottom").style.backgroundColor = "#555555";
+		document.getElementById("top").className = green;
+		document.getElementById("center").className = off;
+		document.getElementById("bottom").className = off;
 	} else {
-		document.getElementById("top").style.backgroundColor = "#555555";
-		document.getElementById("center").style.backgroundColor = "#FF6514";
-		document.getElementById("bottom").style.backgroundColor = "#555555";
+		document.getElementById("top").className = off;
+		document.getElementById("center").className = orange;
+		document.getElementById("bottom").className = off;
 	}
 }
